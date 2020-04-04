13439e9dc946ff26ce266e402f69884caae7e095
kakaxi11 <1099965786@qq.com>
2020-04-04 16:41:41 +0800

    s

---
+++ index.js
@@ -0,0 +1,17 @@
+// var obj = {
+//     'name': '张三',
+//     'age': 18,
+//     'height': 175
+// }
+// var name = obj.name
+// var height = obj.height;
+
+// var { name, height } = obj; //这两种写法都可以进行解构赋值。
+// { name: obj };
+function k(a) {
+    console.log(a);
+}
+
+
+var a = 10;
+k(a);
